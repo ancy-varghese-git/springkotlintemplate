b4d436bc60077042e527f2476f99d3134c17b363
= <=>
2020-09-28 13:17:28 +0530

    Junit changes with properties

--- src/test/kotlin/com/tcs/service/template/ServiceKotlinTemplateApplicationTests.kt
+++ src/test/kotlin/com/tcs/service/template/ServiceKotlinTemplateApplicationTests.kt
@@ -1,13 +1,55 @@
 package com.tcs.service.template
 
-import org.junit.jupiter.api.Test
+import com.tcs.service.template.model.BaseModel
+import io.mockk.MockKAnnotations
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
-class ServiceKotlinTemplateApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ServiceKotlinTemplateApplicationTests(@Autowired private val testRestTemplate: TestRestTemplate) {
 
-	@Test
-	fun contextLoads() {
-	}
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
 
+    @BeforeEach
+    fun `start Test`() {
+        println(">> start test <<")
+        MockKAnnotations.init(this, relaxUnitFun = true)
+    }
+
+    @AfterEach
+    fun `end Test`() {
+        println(">> end test <<")
+    }
+
+    @Test
+    fun `when called should Return BaseModel`() {
+        println("Return BaseModel(1,Basic) object when accessing /api/v1/getModel.....")
+        val result = testRestTemplate.getForEntity("/api/v1/getModel", BaseModel::class.java)
+        assertNotNull(result)
+        assertEquals(result?.statusCode, HttpStatus.OK)
+        assertEquals(result?.body, BaseModel("1", "Basic"))
+    }
+
+    @Test
+    fun contextLoads() {
+        println("CONTEXT LOADED.....")
+    }
+
+    @Test
+    fun `Mockk check`() {
+        println(">> TODO <<")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
+    }
 }
